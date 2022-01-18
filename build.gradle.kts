fcf9778804e338baa9848a0eeb48e1d43608d6a2
Stig Norland <stig.norland@gmail.com>
2022-01-18 18:47:55 +0100

    https://github.com/DASISH/qddt-client/issues/770 https://github.com/DASISH/qddt-client/issues/769

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.5"
+	id("org.springframework.boot") version "2.5.8"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.21"
-	kotlin("plugin.spring") version "1.5.21"
-	kotlin("plugin.jpa") version "1.5.21"
+	kotlin("jvm") version "1.6.10"
+	kotlin("plugin.spring") version "1.6.10"
+	kotlin("plugin.jpa") version "1.6.10"
 }
 
 
@@ -20,33 +20,38 @@ repositories {
 
 dependencies {
 
-	implementation("org.springframework.boot:spring-boot-starter-security:2.5.5")
-	implementation("org.springframework.boot:spring-boot-starter-actuator:2.5.5")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-	implementation("org.springframework.boot:spring-boot-starter-data-rest:2.5.5")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.5.5") {
+//	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	{
 		exclude("org.apache.tomcat:tomcat-jdbc")
 	}
-	implementation("org.springframework.data:spring-data-rest-hal-explorer:3.5.5")
-	implementation("org.springframework.boot:spring-boot-starter-hateoas:2.5.5")
-	implementation("org.springframework.data:spring-data-envers:2.5.5")
+
+//	implementation("org.springframework.data:spring-data-rest-hal-browser")
+	implementation("org.springframework.data:spring-data-rest-hal-explorer")
+	implementation("org.springframework.boot:spring-boot-starter-hateoas")
+	implementation("org.springframework.data:spring-data-envers")
 //	implementation("org.hibernate:hibernate-envers:5.3.11.Final")
 
 	implementation("org.apache.httpcomponents:httpclient:4.5.13")
 	implementation("org.apache.httpcomponents:httpclient-cache:4.5.13")
 
-	implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
 
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
-	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
+//	implementation("com.fasterxml.jackson.core:jackson-core:2.13.1")
+//	implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+//	implementation("org.jetbrains.kotlin:kotlin-reflect")
+//	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 //	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+
 
 	implementation("com.google.code.gson:gson:2.8.9")
 
-	implementation("net.logstash.logback:logstash-logback-encoder:6.6")
+	implementation("net.logstash.logback:logstash-logback-encoder:7.0.1")
 
 //	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
@@ -54,9 +59,10 @@ dependencies {
 
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
-    implementation("com.itextpdf:itext7-core:7.1.16")
+    implementation("com.itextpdf:itext7-core:7.1.17")
     implementation("com.itextpdf:html2pdf:3.0.5")
 
+	implementation("org.apache.commons:commons-lang3:3.12.0")
 
 
 	runtimeOnly("org.postgresql:postgresql")
@@ -65,23 +71,27 @@ dependencies {
 
 	// kapt("org.springframework.boot:spring-boot-configuration-processor")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test:2.5.5") {
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 		exclude(module = "mockito-core")
 	}
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-	testImplementation("com.ninja-squad:springmockk:3.0.1")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+	testImplementation("com.ninja-squad:springmockk:3.1.0")
 
-	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
 }
-	
+
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+		jvmTarget = "11"
 	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
 
 allOpen {
@@ -90,6 +100,4 @@ allOpen {
 	annotation("javax.persistence.MappedSuperclass")
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
+
